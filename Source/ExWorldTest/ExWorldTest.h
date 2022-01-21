187facab811c2550a2f4fba778ab9ec266f4ed3b
Rubén <rubn2113@gmail.com>
2022-01-20 18:48:04 -0600

    Base structure
    Added base classes, enums, variables, and blueprints that are essential in the flow and are going to be used later in the test.

--- Source/ExWorldTest/ExWorldTest.h
+++ Source/ExWorldTest/ExWorldTest.h
@@ -4,3 +4,19 @@
 
 #include "CoreMinimal.h"
 
+/** Effects that can be applied when a projectile hits something. */
+UENUM(BlueprintType)
+enum class EEffectType : uint8 {
+    ET_None                 UMETA(DisplayName = "None"),
+    ET_Destroy              UMETA(DisplayName = "Destroy"),
+    ET_SpawnParticles       UMETA(DisplayName = "SpawnParticles"),
+    ET_AffectAttributes     UMETA(DisplayName = "AffectAttributes"),
+};
+
+/** Objects that can collied with the projectile. */
+UENUM(BlueprintType)
+enum class EObjectType : uint8 {
+    OT_Static               UMETA(DisplayName = "Static"),
+    OT_Pawn                 UMETA(DisplayName = "Pawn"),
+    OT_Destructible         UMETA(DisplayName = "Destructible"),
+};
