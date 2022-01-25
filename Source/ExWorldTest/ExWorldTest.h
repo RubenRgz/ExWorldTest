14208d862e6f02964000b0b0616b56c03ea22c97
Rubén <rubn2113@gmail.com>
2022-01-25 01:55:57 -0600

    Fixed Bug In Shoot Projectile
    The projectile was spwaning multiple times

--- Source/ExWorldTest/ExWorldTest.h
+++ Source/ExWorldTest/ExWorldTest.h
@@ -3,6 +3,8 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "ExWorldTest.generated.h"
 
 /** Effects that can be applied when a projectile hits something. */
 UENUM(BlueprintType)
@@ -19,4 +21,31 @@ enum class EObjectType : uint8 {
     OT_Static               UMETA(DisplayName = "Static"),
     OT_Pawn                 UMETA(DisplayName = "Pawn"),
     OT_Destructible         UMETA(DisplayName = "Destructible"),
+};
+
+/** Structure that defines a level up table entry */
+USTRUCT(BlueprintType)
+struct FEffectData : public FTableRowBase
+{
+    GENERATED_USTRUCT_BODY()
+
+public:
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = LevelUp)
+    EObjectType CollidedTypeObject;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = LevelUp)
+    EEffectType EffectToApply;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = LevelUp)
+    bool IsProjectileDestructibleOnContact;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = LevelUp)
+    bool IsAoEDamage;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = LevelUp)
+    TArray<EObjectType> AffectedTypes;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = LevelUp)
+    int32 NumOfAffected;
 };
